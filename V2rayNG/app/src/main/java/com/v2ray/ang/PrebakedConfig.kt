47f666dc01ba33836dd3ae9bf9e6b0d9928f5572
mamehedin10 <mohi.workzone@gmail.com>
2026-07-22 01:35:17 +0600

    Rebrand to Hera VPN (new package com.hera.vpn) + align baked config to working server

--- V2rayNG/app/src/main/java/com/v2ray/ang/PrebakedConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/PrebakedConfig.kt
@@ -13,10 +13,10 @@ object PrebakedConfig {
     // Pre-loaded server (VLESS + Vision + Reality). Update this string to rotate the server.
     private const val LINK =
         "vless://601413a8-3cb8-4d01-b0fb-85a045ac1881@70.34.215.102:443" +
-            "?encryption=none&flow=xtls-rprx-vision&security=reality" +
+            "?encryption=none&security=reality" +
             "&sni=www.apple.com&fp=chrome" +
             "&pbk=NfdvTSju7RVjAtXLRUNHRLffoRiBWuivCxcwNz6vDjI" +
-            "&sid=6ba85179e30d4fc2&type=tcp&headerType=none#RU-VPN"
+            "&sid=6ba85179e30d4fc2&type=tcp&headerType=none#Hera-VPN"
 
     /**
      * Imports the baked profile and marks it selected if no servers exist yet.
